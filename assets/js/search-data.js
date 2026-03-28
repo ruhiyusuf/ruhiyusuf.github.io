b880d08383fcd3fdc7701fc266db4f0149ae9bf1
ruhiyusuf <77863500+ruhiyusuf@users.noreply.github.com>
2026-03-28 07:15:07 +0000

    Deploying to gh-pages from @ ruhiyusuf/ruhiyusuf.github.io@a92b97934327bdb3d1d83b03a7cad4b9696cba1b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -76,7 +76,7 @@ ninja.data = [{
               window.location.href = "/projects/4_project/";
             },},{id: "projects-enhancing-soliton-microcomb-stability-with-custom-waveform-control",
           title: 'Enhancing Soliton Microcomb Stability with Custom Waveform Control',
-          description: "Developed a Red Pitaya–based control system that generates tailored pump-laser detuning waveforms to reliably trigger and extend soliton steps in Si₃N₄ microresonators. This approach created longer, more stable pulse trains—improving optical frequency comb coherence and enabling integration into real-world photonic systems used in metrology, communications, and quantum sensing.",
+          description: "Developed a Red Pitaya–based waveform control system for pump-laser detuning in Si₃N₄ microring resonators. Performed dispersion measurements, mode analysis, and resonance fitting, and implemented tuning strategies for more reliable soliton generation and improved frequency comb stability.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/5_project/";
             },},{id: "projects-driver-camera-and-retroreflective-vision-jetson-nano",
