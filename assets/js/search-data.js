58fe33fa1f282b60a1f72ec158b383d67b8d4a8c
ruhiyusuf <77863500+ruhiyusuf@users.noreply.github.com>
2026-03-28 06:58:45 +0000

    Deploying to gh-pages from @ ruhiyusuf/ruhiyusuf.github.io@5ed3b0bb9342d0899d5fa0efc54afd67f087a3dd 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -66,7 +66,7 @@ ninja.data = [{
               window.location.href = "/projects/2_project/";
             },},{id: "projects-balanced-homodyne-photodetector-pcb",
           title: 'Balanced Homodyne Photodetector PCB',
-          description: "Designed and fabricated a low-noise balanced photodetector for homodyne detection using LTSpice and EasyEDA. Characterized noise performance, gain, and bandwidth, achieving performance comparable to commercial systems at significantly reduced cost.",
+          description: "Designed and fabricated a low-noise balanced photodetector for homodyne detection in quantum optics experiments, focusing on analog circuit design, PCB layout, and optical testing.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
             },},{id: "projects-esp32-liquid-nitrogen-weight-monitoring-system",
