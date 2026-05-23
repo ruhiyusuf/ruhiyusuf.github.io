37209612d37f4ac894eca0623fbb058637b34053
ruhiyusuf <77863500+ruhiyusuf@users.noreply.github.com>
2026-05-23 05:42:07 +0000

    Deploying to gh-pages from @ ruhiyusuf/ruhiyusuf.github.io@0a36e5a8d16c91541095728dca37b02f3a07a72e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,7 +9,14 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-projects",
+  },{id: "nav-news",
+          title: "news",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/news/";
+          },
+        },{id: "nav-projects",
           title: "projects",
           description: "A selection of research and engineering projects.",
           section: "Navigation",
