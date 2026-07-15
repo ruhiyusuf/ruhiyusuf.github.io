04eafd1992c121c6f0e35f780b765b5d241eb03d
ruhiyusuf <77863500+ruhiyusuf@users.noreply.github.com>
2026-07-15 05:21:05 +0000

    Deploying to gh-pages from @ ruhiyusuf/ruhiyusuf.github.io@7963e16ade8241af51075233014a62354ac93892 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -135,6 +135,13 @@ ninja.data = [{
           window.open("https://www.linkedin.com/in/ruhiy", "_blank");
         },
       },{
+        id: 'social-scholar',
+        title: 'Google Scholar',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://scholar.google.com/citations?user=-iiwmKsAAAAJ", "_blank");
+        },
+      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
